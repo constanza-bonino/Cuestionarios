4ab39322071c8b4f2cd7be84d936699ff95b9aee
Felipe Alonso <alonso.felipe23@gmail.com>
2025-05-19 20:00:22 -0300

    titulo

--- Cuestionarios/src/pages/ListaPreguntasPage/ListaPreguntasPage.jsx
+++ Cuestionarios/src/pages/ListaPreguntasPage/ListaPreguntasPage.jsx
@@ -8,6 +8,7 @@ function ListaPreguntasPage() {
 
     const params = useParams();
 
+    const [cuestionario, setCuestionario] = useState(null);
     const [preguntas, setPreguntas] = useState(null);
 
     useEffect(() => {
@@ -17,22 +18,33 @@ function ListaPreguntasPage() {
                 const url = `http://localhost:3000/preguntas/?id_cuestionario=${params.idCuestionario}`;
                 const res = await fetch(url);
                 const data = await res.json();
-                console.log('Data: ', data);
                 setPreguntas(data);
             } catch (err) {
                 console.error("Failed to fetch:", err);
             }
         };
 
+        const fetchDataCuestionario = async () => {
+            try {
+                const url = `http://localhost:3000/cuestionarios/${params.idCuestionario}`;
+                const res = await fetch(url);
+                const data = await res.json();
+                setCuestionario(data);
+            } catch (err) {
+                console.error("Failed to fetch:", err);
+            }
+        }
+
         fetchQs();
+        fetchDataCuestionario();
     }, [params.idCuestionario]);
 
     // We want to render some loading state if the product is not yet loaded 👇
-    if (!preguntas) return <p>Cargando preguntas...</p>;
+    if (!preguntas || !cuestionario) return <p>Cargando preguntas...</p>;
     console.log("Preguntas: ", preguntas);
     // We want to render the product details 👇
     return (<div className="preguntas">
-        <h1>{preguntas.nombre}</h1>
+        <h1>{cuestionario.nombre}</h1>
         {
             preguntas.map((pregunta) => {console.log(pregunta); return (
                 <Link key={"c" + params.idCuestionario + "p" + pregunta.id} to={"/cuestionarios/" + params.idCuestionario + "/" + pregunta.id}> {pregunta.nombre} </Link>
